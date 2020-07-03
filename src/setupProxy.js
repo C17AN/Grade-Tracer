69f1e829735463cba185017ecdd242e014fa53d2
c17an <kimchanmin1@gmail.com>
2020-07-03 18:53:18 +0900

    changeOrigin added

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app) => {
   app.use(
-    createProxyMiddleware("/", {
-      target: "http://localhost:5000/",
+    createProxyMiddleware("/grade", {
+      target: "http://localhost:5000/grade",
       changeOrigin: true,
     })
   );
