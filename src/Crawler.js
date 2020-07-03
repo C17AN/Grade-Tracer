98fbda5eb288c7bcf9217508a0b8287e37fb015b
c17an <kimchanmin1@gmail.com>
2020-07-03 20:17:41 +0900

    headless false

--- src/Crawler.js
+++ src/Crawler.js
@@ -48,7 +48,7 @@ const getSubjectGrade = async (page) => {
 
 const Crawler = async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
