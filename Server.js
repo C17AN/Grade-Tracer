162afc9e28f220ea85afd3c9ec778b02f5d6fc19
c17an <kimchanmin1@gmail.com>
2020-07-03 16:38:52 +0900

    fetch URL changed

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { crawler } = require("./src/Crawler");
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 let subject = [];
 const init = async () => {
